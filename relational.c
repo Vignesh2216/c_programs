f34c86f144f03902a4d641e4d855ae4cc42d9c43
Vignesh2216 <b.s.vignesh22012006@gmail.com>
2023-09-16 20:37:15 +0530

    relational

---
+++ relational.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a=5,b=5,c=3,d=8;
+    printf("%d",a==b);
+    printf("\n%d",a!=c);
+    printf("\n%d",a>d);
+    printf("\n%d",b>c);
+    return 0;
+}
