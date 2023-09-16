ee3c73281e6287db6009fcdacb3e3dc3855930a1
Vignesh2216 <b.s.vignesh22012006@gmail.com>
2023-09-16 20:56:16 +0530

    if else

---
+++ ifelse.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b;
+    printf("enter a:");
+    scanf("%d",&a);
+    printf("enter b:");
+    scanf("%d",&b);
+    if(a<b)
+    {
+        printf("b is greater");
+    }
+    else
+    {
+        printf("a is grater");
+    }
+    return;
+}
