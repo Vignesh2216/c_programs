3596bf794409726863152f66a5e7a8500f0eda6a
Vignesh2216 <b.s.vignesh22012006@gmail.com>
2023-09-16 20:14:36 +0530

    arithmetics

---
+++ arithmetic.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a=5 , b=10;
+    int sum,diff,mul,div;
+    sum=a+b;
+    diff=b-a;
+    mul=a*b;
+    div=b/a;
+    printf("sum=""%d",sum);
+    printf("\n""diff=""%d",diff);
+    printf("\n""mul=""%d",mul);
+    printf("\n""div=""%d",div);
+    getch();
+}
