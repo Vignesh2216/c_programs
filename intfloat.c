6e367fba751a1b377ca768376fcd65f7e7eb371e
Vignesh2216 <b.s.vignesh22012006@gmail.com>
2023-09-16 20:26:44 +0530

    intfloat

--- intfloat.c
+++ intfloat.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 #include<conio.h>
-void main()
+int main()
 {
     int a=5;
     float b=1.2;   
     printf("a=""%d",a);
     printf("\nb=""%f",b);
-    getch();
+    return 0;
 }
