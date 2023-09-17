4b40ee255fac5236159da2047a178a2d54e28772
Vignesh2216 <b.s.vignesh22012006@gmail.com>
2023-09-17 11:16:33 +0530

    reverse

---
+++ reversenumber.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n,copy,rev=0;
+    printf("enter n:");
+    scanf("%d",&n);
+    copy=n;
+
+    while(copy>0)
+    {
+        rev=rev*10;
+        rev=rev+(copy%10);
+        copy=copy/10;
+    }
+    printf("reverse:%d",rev);
+    return;
+}
