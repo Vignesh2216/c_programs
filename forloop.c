8593ad844ed0dad537c2713a176d5bd1bea72408
Vignesh2216 <b.s.vignesh22012006@gmail.com>
2023-09-17 10:07:33 +0530

    forloop

---
+++ forloop.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int i,n,sum=0;
+    printf("enter value of n:");
+    scanf("%d",&n);
+
+    for(i=1; i<=n; i++)
+    {
+        sum=sum+i;
+    }
+    printf("sum:%d",sum);
+    return;
+}
