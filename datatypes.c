4e091c922af7378f35019e4a587137a3fdedac7e
Vignesh2216 <b.s.vignesh22012006@gmail.com>
2023-09-20 15:35:11 +0530

    datatypes

---
+++ datatypes.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a=5;
+    float b=1.5;
+    char c='x';
+    double d=1.76;
+
+    printf("%d\n",a);
+    printf("%f\n",b);
+    printf("%c\n",c);
+    printf("%lf\n",d);
+}
